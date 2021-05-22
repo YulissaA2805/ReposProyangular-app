910e9c26f9fa2d7417e7a02b4dc0d1c53872f313
YulissaA2805 <yulissa.alvarez@cetys.edu.mx>
2021-05-22 00:47:05 -0700

    1er commit

---
+++ src/routes/api.js
@@ -0,0 +1,23 @@
+const {Router} = require('express');
+const router = Router();
+
+const productos = require('./datos.json');
+console.log(productos);
+
+router.get('/', (req,res) => {
+    res.json(productos);
+});
+
+router.get('/:id', (req,res) => {
+    const {id} = req.params;
+    productos.forEach(producto => {
+        if(producto.id == id){
+            res.json(producto);
+        }
+    });
+    console.log(id);
+});
+
+
+
+module.exports = router;
