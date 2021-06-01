349109a17b7ba16b7ad4fbed8ea8bb90b62778a9
LudmilaHaro <ludmilaharo@outlook.com>
2021-06-01 13:58:38 -0700

    Cambio en datosNotas

--- src/models/notas.js
+++ src/models/notas.js
@@ -1,6 +1,7 @@
 const {Schema, model} = require('mongoose')
 const NotaSchema = new Schema({
     text:{type:String, required:true}
+    //,color:{type:String, required:true}
 },{
     timestamps: true, //agregar tiempo de creacion y modificacion
     versionKey: false //
